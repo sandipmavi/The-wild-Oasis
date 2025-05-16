e6ad6561ce36dac1f04bc41c8ae57122a14d4c28
Sandip Mavi <gurjarsandeep9897@gmail.com>
2025-05-16 09:36:21 +0530

    Implemented dashboard

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("mavi@g.com");
-  const [password, setPassword] = useState("123456");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
   function handleSubmit(e) {
     e.preventDefault();
